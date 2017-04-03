520bdb13a31136df051a2af1511b0046b703df43
Ryan Workman <rdavid1099@gmail.com>
2017-04-03 17:47:02 -0600

    Changing array implementation to be more of a linked-list

--- lib/array.rb
+++ lib/array.rb
@@ -1,5 +1,23 @@
 module Ry
   class Array
+    def initialize(count = 0, initial_data = 'nil')
+      @content = count > 0 ? initialize_array(count, initial_data) : '[]'
+    end
 
+    def initialize_array(count, initial_data, i = 0, result = '[')
+      while i < count
+        result += i == count - 1 ? "#{initial_data}]" : "#{initial_data}, "
+        i += 1
+      end
+      result
+    end
+
+    def [](index)
+      
+    end
+
+    def inspect
+      @content
+    end
   end
 end
